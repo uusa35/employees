12501a63eeb3805caaaa16a7a6b0bff8fdd1f2f0
uusa35 <uusa35@gmail.com>
2021-01-28 08:38:20 +0300

    updates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">قائمة رواتب الموظفين</div>
+                    <div class="card-header text-center text-lg-center">قائمة رواتب الموظفين</div>
 
                     <div class="card-body">
                         @if (session('status'))
